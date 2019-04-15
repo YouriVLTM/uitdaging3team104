4981d0eae16815a2fcb5e218520978bb962986b8
YouriVLTM <r0694461@student.thomasmore.be>
2019-04-15 15:02:41 +0200

    Overzicht pagina

--- public_html/overzicht.php
+++ public_html/overzicht.php
@@ -7,41 +7,95 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Overzicht pagina</title>
     <style>
-        /* Always set the map height explicitly to define the size of the div
-           * element that contains the map. */
-
-        #map {
-            height: 100%;
-        }
-
-        /* Optional: Makes the sample page fill the window. */
-
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-
+    #mapid {
+    width: 100%;
+    height: 500px;
+    margin: 0;
+    }
     </style>
+    <!--Maps-->
+    <!-- Leaflet -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+
+    <!-- Mapbox GL -->
+    <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.35.1/mapbox-gl.css" rel='stylesheet' />
+    <script src="https://api.tiles.mapbox.com/mapbox-gl-js/v0.35.1/mapbox-gl.js"></script>
 
 </head>
-<body>
+<body class="background-img">
 <!--Nav-->
+<nav class="navbar navbar-expand-lg navbar-light bg-success ml-auto">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Benodigdheden</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Overzicht</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">extreem geocaching</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nieuws</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
 
 <!--END nav-->
-
-<!-- Container -->
-<section class="py-6 bg-gray-100 background-img">
+<section class="background3-img">
     <div class="container">
-        <div class="row mb-5">
-            <div class="col-md-8">
-                <h2>All locations</h2>
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="vertical" style="color:white;margin-top:76px;margin-button:76px;">Vind al u geo plaatsen</h1>
             </div>
-            <div class="col-md-4 d-md-flex align-items-center justify-content-end"><a href="blog.html" class="text-muted text-sm">
-
-                See all articles<i class="fas fa-angle-double-right ml-2"></i></a></div>
         </div>
+    </div>
+</section>
+<section class="mt-4 bg-gray-100">
+    <div class="container-fluid">
+         <div id="mapid"></div>
+     </div>
+ </section>
+
+ <!-- Container -->
+<section class="py-6 bg-gray-100">
+    <div class="container">
+
+        <div class="row mt-5 mb-5">
+            <div class="col">
+                <h1>Vind al u geo plaatsen</h1>
+            </div>
+        </div>
+
+
+
+
         <div class="row">
             <?php
 
@@ -56,46 +110,55 @@
 
             ?>
             <?php foreach ($json_data as $point): ?>
-            <!-- blog item-->
-            <div class="col-lg-4 col-sm-6 mb-4">
-                <div class="card shadow border-0 h-100"><img src="https://d19m59y37dris4.cloudfront.net/directory/1-1/img/photo/photo-1512917774080-9991f1c4c750.jpg" alt="..." class="img-fluid card-img-top">
-                    <div class="card-body">
-                        <h5 class="my-2"><?php echo $point["Name"];?></h5>
-                        <p class="text-gray-500 text-sm my-3"><i class="far fa-clock mr-2"></i><?php echo $point["Date"]["LastVisited"]; ?></p>
-
-                        <div class="row">
-                            <div class="col-4">Moeilijk : </div>
-                            <div class="col-8 p-1">
-                                <div class="progress">
-                                    <div class="progress-bar bg-warning" role="progressbar" style="width: <?php echo $point["Quality"]["Difficult"]*10;?>%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10"><?php echo $point["Quality"]["Difficult"];?></div>
+                <!-- blog item-->
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card shadow border-0 h-100"><img src="img<?php echo $point["PictureUrl"];?>" alt="..." class="img-fluid card-img-top">
+                        <div class="card-body">
+                            <div class="clearfix">
+                                <div class="float-left">
+                                    <h5><?php echo $point["Name"];?></h5>
+                                </div>
+                                <div class="float-right">
+                                    <i class="fas fa-heart pr-2"></i><?php echo $point["visitors"];?>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-4">Size : </div>
-                            <div class="col-8 p-1">
-                                <div class="progress">
-                                    <div class="progress-bar bg-info" role="progressbar" style="width: <?php echo $point["Quality"]["Size"]*10;?>%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10"><?php echo $point["Quality"]["Size"];?></div>
+
+
+                            <p class="text-gray-500 text-sm my-3"><i class="far fa-clock mr-2"></i><?php echo $point["Date"]["LastVisited"]; ?></p>
+
+                            <div class="row">
+                                <div class="col-4">Moeilijk : </div>
+                                <div class="col-8 p-1">
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: <?php echo $point["Quality"]["Difficult"]*10;?>%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10"><?php echo $point["Quality"]["Difficult"];?></div>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-4">Size : </div>
+                                <div class="col-8 p-1">
+                                    <div class="progress">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: <?php echo $point["Quality"]["Size"]*10;?>%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10"><?php echo $point["Quality"]["Size"];?></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p>Categorie : <span class="text-success"><?php echo $point["Quality"]["Categorie"];?></span></p>
+
+                            <p>
+                                <!--bereikbaarheid-->
+                                <?php foreach($point["Quality"]["Reachability"] as $Reachability):?>
+                                    <a data-toggle="tooltip" data-placement="top" title="<?php echo $Reachability["Name"]; ?>"><i class="tooltipIcon <?php echo $Reachability["Icon"]; ?> fa-lg"></i></a>
+                                <?php endforeach ?>
+                            </p>
+
+
+                            <p class="my-2 text-muted text-sm"><?php echo substr($point["Description"],0,40) . "<br>" . "..."; ?></p>
+                            <a href="<?php echo 'point.php?pointId=' . $point["Id"]; ?>"  class="btn btn-link pl-0">Lees meer<i class="fa fa-long-arrow-alt-right ml-2"></i></a>
+
                         </div>
-
-                        <p>kategorie : <span class="text-success"><?php echo $point["Quality"]["Categorie"];?></span></p>
-
-                        <p>
-                        <!--bereikbaarheid-->
-                        <?php foreach($point["Quality"]["Reachability"] as $Reachability):?>
-                            <a data-toggle="tooltip" data-placement="top" title="<?php echo $Reachability["Name"]; ?>"><i class="tooltipIcon <?php echo $Reachability["Icon"]; ?> fa-lg"></i></a>
-                        <?php endforeach ?>
-                        </p>
-
-
-                        <p class="my-2 text-muted text-sm"><?php echo $point["Description"]; ?></p>
-                        <a href="<?php echo 'point.php?pointId=' . $point["Id"]; ?>"  class="btn btn-link pl-0">Read more<i class="fa fa-long-arrow-alt-right ml-2"></i></a>
-
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
 
 
@@ -113,31 +176,39 @@
 <!--<div id="map"></div>-->
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/leaflet-mapbox-gl.js"></script>
 <script>
-    $(document).ready(function() {
-        $('[data-toggle="tooltip"]').tooltip();
+    var map = L.map('mapid', {
+        center: [51.142467, 5.080317],
+        zoom: 13
     });
 
+    var defaultIcon = L.icon({
+        iconUrl: 'img/pot.png',
+        iconSize: [15, 35],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
+    });
+
+
+    <?php foreach ($json_data as $point): ?>
+
+
+L.marker([<?php echo $point["Location"]["Latitude"]; ?>, <?php echo $point["Location"]["Longitude"]; ?>],
+    {icon: defaultIcon}
+    )
+    .bindPopup("<h3><?php echo $point["Name"]; ?></h3><br><img src='img<?php echo $point["PictureUrl"];?>' style='height:130px' /><br><br><a href='point.php?pointId=<?php echo $point["Id"]; ?>'>Lees meer</a>")
+            .addTo(map);
+
+        <?php endforeach; ?>
+
+var gl = L.mapboxGL({
+    accessToken: 'no-token',
+    style: 'https://raw.githubusercontent.com/osm2vectortiles/mapbox-gl-styles/master/styles/bright-v9-cdn.json'
+}).addTo(map);
+
 </script>
-<script>
 
 
-    var map;
-
-    function initMap()
-    {
-        map = new google.maps.Map(document.getElementById('map'),
-            {
-                center:
-                    {
-                        lat: -34.397,
-                        lng: 150.644
-                    },
-                zoom: 8
-            });
-    }
-
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9GAkK2CH8lMbGzycuo3EXIUZmErH99vs&callback=initMap" async defer></script>
 </body>
 </html>
--- public_html/point.php
+++ public_html/point.php
@@ -7,34 +7,102 @@
     <link rel="stylesheet" href="css/style.css">
     <title>1ITF Sass BS4 project</title>
 </head>
-<body>
+<body class="background2-img">
 
+<!--Nav-->
+<nav class="navbar navbar-expand-lg navbar-light bg-success ml-auto">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Benodigdheden</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Overzicht</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">extreem geocaching</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nieuws</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+
+<!--END nav-->
 
 <?php if(isset($_GET["pointId"])): ?>
-    <h1>request ok</h1>
+
+    <?php
+    $Id = $_GET["pointId"];
+
+    // Read JSON file
+    $json = file_get_contents('./data.json');
+
+    //Decode JSON
+    $json_data = json_decode($json,true);
+    //kijken of json $Id bestaat
+
+
+    if($Id>0 && sizeof($json_data)>$Id):
+        $point = $json_data[$Id];
+
+    ?>
+
     <!-- Container -->
-    <section class="py-6 bg-gray-100">
+    <section class="py-6 bg-gray-100 mt-5">
         <div class="container">
+            <div class="card shadow border-0 h-100">
+                <div class="card-body">
 
+                    <div class="clearfix">
+                        <div class="float-left">
+                            <a href="overzicht.php"><i class="fa fa-long-arrow-alt-left mr-2"></i>Back to</a>
+                        </div>
+                        <div class="float-right">
+                            <i class="fas fa-heart pr-2"></i><?php echo $point["visitors"];?>
+                        </div>
+                    </div>
 
-            <?php
-            $Id = $_GET["pointId"];
-
-            // Read JSON file
-            $json = file_get_contents('./data.json');
-
-            //Decode JSON
-            $json_data = json_decode($json,true);
-            $point = $json_data[$Id];
-
-            ?>
-
-            <div class="row">
-                <div class="col-4">
-                    <img src="img/test.jpg" alt="" style="width:100%">
+            <div class="row mt-3">
+                <div class="col-sm-12 col-md-4 ">
+                    <img class="rounded img-fluid" src="img/<?php echo $point["PictureUrl"];?>" alt="">
                 </div>
-                <div class="col-8">
-                    <h2><?php echo $point["Name"]; ?> </h2>
+                <div class="col-sm-12 col-md-8 ">
+                    <div class="d-flex flex-row bd-highlight mb-3 align-items-center">
+                        <div class="p-2 bd-highlight">
+                            <img src="img/pot.png" alt="" style="height:50px">
+                        </div>
+                        <div class="p-2 bd-highlight">
+                            <h2><?php echo $point["Name"]; ?> </h2>
+                        </div>
+                    </div>
+
+
                     <p><?php echo $point["Description"]; ?> </p>
                     <hr>
 
@@ -59,21 +127,91 @@
 
                     <p>kategorie : <span class="text-success"><?php echo $point["Quality"]["Categorie"];?></span></p>
 
+                    <p>
+                        <!--bereikbaarheid-->
+                        <?php foreach($point["Quality"]["Reachability"] as $Reachability):?>
+                            <a data-toggle="tooltip" data-placement="top" title="<?php echo $Reachability["Name"]; ?>"><i class="tooltipIcon <?php echo $Reachability["Icon"]; ?> fa-lg"></i></a>
+                        <?php endforeach ?>
+                    </p>
 
                 </div>
 
 
 
             </div>
+
+
+
+            <div class="row">
+                <div class="col-sm-12 col-md-8">
+                    <h2>Locatie</h2>
+                    <hr>
+
+                    <div class="d-flex flex-row bd-highlight mb-2">
+                        <div class="p-2 bd-highlight">
+                            <p><code><?php echo $point["Location"]["Latitude"]; ?>, <?php echo $point["Location"]["Longitude"]; ?></code></p>
+                        </div>
+                        <div class="p-2 bd-highlight">
+                            <p><?php echo $point["Location"]["Stad"]; ?> </p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+           <div class="row">
+               <div class="col-sm-12 col-md-8">
+                   <h2>Reviews</h2>
+                   <hr>
+                    <div class="row">
+                        <div class="col-sm-2 col-lg-1">
+                            <p><i class="fas fa-user-circle fa-3x"></i></p>
+                        </div>
+                        <div class="col-sm-10 col-lg-11">
+                            <p>Youri Van Laer <span>2 min geleden</span></p>
+                            <p>Deze gelogd tijdens het rondje Nature & Adventure</p>
+                        </div>
+                    </div>
+
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-2 col-lg-1">
+                            <p><i class="fas fa-user-circle fa-3x"></i></p>
+                        </div>
+                        <div class="col-sm-10 col-lg-11">
+                            <p>Youri Van Laer <span>2 min geleden</span></p>
+                        </div>
+                    </div>
+                </div>
+           </div>
+
+                </div>
+            </div>
         </div>
+
     </section>
     <!--End Container-->
 
 
+    <?php else: ?>
 
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Error</h1>
+                <p class="lead">Sorry deze pagina is niet gevonden!</p>
+            </div>
+        </div>
+
+    <?php endif; ?>
 
 <?php else: ?>
-    <h1>Sorry de pagina is niet ok!</h1>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Error</h1>
+            <p class="lead">Sorry get request is niet gevonden!</p>
+        </div>
+    </div>
 
 
 <?php endif; ?>
@@ -87,6 +225,13 @@
 
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+</script>
 <!--custom scripts-->
 </body>
 </html>
