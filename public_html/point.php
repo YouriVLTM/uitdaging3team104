9275bf1854417518f98defac63963e4e19766253
YouriVLTM <r0694461@student.thomasmore.be>
2019-04-15 20:17:32 +0200

    image

--- public_html/point.php
+++ public_html/point.php
@@ -10,7 +10,7 @@
 <body class="background2-img">
 
 <!--Nav-->
-<nav class="navbar navbar-expand-lg navbar-light bg-success ml-auto">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary ml-auto">
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -68,7 +68,7 @@
     //kijken of json $Id bestaat
 
 
-    if($Id>0 && sizeof($json_data)>$Id):
+    if($Id>=0 && sizeof($json_data)>$Id):
         $point = $json_data[$Id];
 
     ?>
