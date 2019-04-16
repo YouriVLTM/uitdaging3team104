d5a40037001ad3833135c8e6c7caa8f5c17fb87b
YouriVLTM <r0694461@student.thomasmore.be>
2019-04-16 19:53:21 +0200

    make logo + change navbar

--- public_html/point.php
+++ public_html/point.php
@@ -10,31 +10,34 @@
 <body class="background2-img">
 
 <!--Nav-->
-<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-primary ml-auto">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <img src="logo/logo2.svg" alt="" style="height:50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Benodigdheden</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="overzicht.php">Overzicht</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="extreem.html">extreem geocaching</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nieuws</a>
+                </li>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Benodigdheden</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="overzicht.php">Overzicht</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="extreem.html">extreem geocaching</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Nieuws</a>
-            </li>
-
-        </ul>
-
+            </ul>
+        </div>
     </div>
 </nav>
 
